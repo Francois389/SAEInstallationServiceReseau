5561aebc0497c40735039e164f6f94a0176cf7c2
quentinou-le-nul <quentin161204@gmail.com>
2023-04-05 11:08:27 +0200

    avancée sur le js pour les formulaires

--- website/src/js/main.js
+++ website/src/js/main.js
@@ -1,10 +1,64 @@
 const buttonRedirectLogin = document.getElementById('buttonRedirectLogin');
 const buttonRedirectSignUp = document.getElementById('buttonRedirectSignUp');
+const buttonBack = document.getElementById('back');
+const buttonSend = document.getElementById('send');
+const inputMailSignUp = document.getElementById('mailSignUp');
+const inputNameSignUp = document.getElementById('nameSignUp');
+const inputFistNameSignUp = document.getElementById('firstNameSignUp');
+const inputPasswordSignUp = document.getElementById('passwordSignUp');
 
-buttonRedirectLogin.addEventListener('click', () => {
+
+if (buttonBack !== null) {
+    buttonBack.addEventListener('click', () => {
+        window.location.href = '../racine.html';
+    });
+};
+
+if (buttonRedirectLogin !== null || buttonRedirectSignUp !== null) {
+    buttonRedirectLogin.addEventListener('click', () => {
     window.location.href = 'pages/connexion.html';
-});
+    });
+};
 
-buttonRedirectSignUp.addEventListener('click', () => {
+if (buttonRedirectSignUp !== null || buttonRedirectLogin !== null) {
+    buttonRedirectSignUp.addEventListener('click', () => {
     window.location.href = 'pages/inscription.html';
-});
+    });
+};
+
+if (buttonSend !== null) {
+    buttonSend.addEventListener('click', validateFormSignUp);
+};
+
+if (inputPasswordSignUp !== null) {
+    if (inputPasswordSignUp.value !== '' && inputMailSignUp.value !== '' && inputNameSignUp.value !== '' && inputFistNameSignUp.value !== '') {
+        validateFormSignUp();
+    } 
+};
+
+/* test de la validitée du formulaire d'incription afin de laisser passer ou non l'utilisateur en autorisant le clic sur le bouton d'envoi du formulaire */
+function validateFormSignUp() {
+    var mail = inputMailSignUp.value;
+    var name = inputNameSignUp.value;
+    var firstName = inputFistNameSignUp.value;
+    var password = inoutPasswordSignUp.value;
+    var mailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    var nameRegex = /^[a-zA-Z]+$/;
+
+    if (mailRegex.test(mail) && nameRegex.test(name) && nameRegex.test(firstName) && password.length >= 8) {
+        buttonSend.style.backgroundColor = '#2ECC71';
+        return true;
+    };
+    if (mailRegex.test(mail) === false) {
+        inputMailSignUp.style.borderColor = 'red';
+    }
+    if (nameRegex.test(name) === false) {
+        inputNameSignUp.style.borderColor = 'red';
+    }
+    if (nameRegex.test(firstName) === false) {
+        inputFistNameSignUp.style.borderColor = 'red';
+    }
+    if (password.length < 8) {
+        inputPasswordSignUp.style.borderColor = 'red';
+    }
+};
