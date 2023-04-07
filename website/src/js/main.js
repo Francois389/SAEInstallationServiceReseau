2f06b6576216901854ee444f0aeaf4d4516a8a8a
quentinou-le-nul <quentin161204@gmail.com>
2023-04-07 13:48:13 +0200

    correction du footer, fin de la page d'incription, début de la page de connexion

--- website/src/js/main.js
+++ website/src/js/main.js
@@ -4,8 +4,9 @@ const buttonBack = document.getElementById('back');
 const buttonSend = document.getElementById('send');
 const inputMailSignUp = document.getElementById('mailSignUp');
 const inputNameSignUp = document.getElementById('nameSignUp');
-const inputFistNameSignUp = document.getElementById('firstNameSignUp');
+const inputFistnameSignUp = document.getElementById('firstnameSignUp');
 const inputPasswordSignUp = document.getElementById('passwordSignUp');
+
 
 if (buttonSend !== null) {
     buttonSend.disabled=true;
@@ -29,33 +30,54 @@ if (buttonRedirectSignUp !== null || buttonRedirectLogin !== null) {
     });
 }
 
-if (buttonSend !== null) {
-    buttonSend.addEventListener('mouseup', validateFormSignUp);
+if (inputFistnameSignUp !== null) {
+    inputPasswordSignUp.addEventListener('blur', validateFormSignUp);
+}
+
+if (inputMailLogin !== null) {
+    inputMailLogin.addEventListener('blur', validateFormLogin);
 }
 
 function validateFormSignUp() {
-    console.log('test');
     const mail = inputMailSignUp.value,
-        name = inputNameSignUp.value,
-        firstName = inputFistNameSignUp.value,
-        password = inputPasswordSignUp.value,
-        mailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-        nameRegex = /^[a-zA-Z]+$/;
+          firstName = inputFistnameSignUp.value,
+          name = inputNameSignUp.value,
+          password = inputPasswordSignUp.value,
+          mailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+          nameRegex = /^[a-zA-Z]+$/;
 
     if (mailRegex.test(mail) && nameRegex.test(name) && nameRegex.test(firstName) && password.length >= 8) {
-        buttonSend.style.backgroundColor = '#2ECC71';
+        buttonSend.style.backgroundColor = '#4158D0';
         buttonSend.disabled=false;
     }
-    if (mailRegex.test(mail) === false) {
+    if (!mailRegex.test(mail)) {
+        console.log('mailRegex.test(mail) : ',inputMailSignUp.value, mailRegex.test(mail));
         inputMailSignUp.style.borderColor = 'red';
+        inputMailSignUp.style.borderWidth = '3px';
+        buttonSend.style.backgroundColor = 'grey';
+        buttonSend.disabled=true;
     }
-    if (nameRegex.test(name) === false) {
+    if (!nameRegex.test(name)) {
+        console.log('nameRegex.test(name) : ',inputNameSignUp.value, nameRegex.test(name));
         inputNameSignUp.style.borderColor = 'red';
+        inputNameSignUp.style.borderWidth = '3px';
+        buttonSend.style.backgroundColor = 'grey';
+        buttonSend.disabled=true;
     }
-    if (nameRegex.test(name) === false) {
-        inputFistNameSignUp.style.borderColor = 'red';
+    if (!nameRegex.test(firstName)) {
+        console.log('nameRegex.test(name) : ',inputFistnameSignUp.value, nameRegex.test(name));
+        inputFistnameSignUp.style.borderColor = 'red';
+        inputFistnameSignUp.style.borderWidth = '3px';
+        buttonSend.style.backgroundColor = 'grey';
+        buttonSend.disabled=true;
     }
     if (password.length < 8) {
+        console.log('password.length < 8 : ', password.length < 8);
         inputPasswordSignUp.style.borderColor = 'red';
+        inputPasswordSignUp.style.borderWidth = '3px';
+        buttonSend.style.backgroundColor = 'grey';
+        buttonSend.disabled=true;
     }
 }
+
+
