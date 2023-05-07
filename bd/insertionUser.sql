41f5838d7bbe54951263e13a3733c2b05c93373e
Francois SP <francois-sp@gmx.fr>
2023-05-07 19:42:21 +0200

    Création du fichier pour insérer les première entrées

---
+++ bd/insertionUser.sql
@@ -0,0 +1,7 @@
+INSERT INTO sos_ordi.personnel (adresse_mail, nom, prenom, mot_de_passe, statut) VALUES
+('alberge@sos_ordi.fr' , 'Alberge' , 'Angelique' , 'vTdAY73j', 1),
+('bertrand@sos_ordi.fr', 'Bertrand', 'Jean'      , 'tyEHac6s', 2),
+('castel@sos_ordi.fr'  , 'Castel'  , 'Martin'    , 'RBJRxqye', 2),
+('dupont@sos_ordi.fr'  , 'Dupont'  , 'Marie'     , 'BpMqT8sB', 3),
+('ferre@sos_ordi.fr'   , 'Ferre'   , 'Sophie'    , '8Y7X6Z4s', 3),
+('guillot@sos_ordi.fr' , 'Guillot' , 'Pierre'    , 'pj7BeXvj', 3);
